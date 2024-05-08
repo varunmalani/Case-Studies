ec2255010f71d7f051f3f0d24d726df31a4c8a2e
Varun Malani <51241123+varunmalani@users.noreply.github.com>
2024-05-09 01:09:54 +0530

    Add files via upload

---
+++ Zomato/Zomato_Cleaning.sql
@@ -0,0 +1,28 @@
+USE DATA_SCIENCE;
+
+CREATE TABLE zomato (
+	a INTEGER NOT NULL, 
+	name VARCHAR(255) NOT NULL, 
+	online_order VARCHAR(255) NOT NULL, 
+	book_table VARCHAR(255) NOT NULL, 
+	rating DECIMAL(4,2) NOT NULL, 
+	votes INTEGER NOT NULL, 
+	location VARCHAR(255) NOT NULL, 
+	rest_type VARCHAR(255), 
+	dish_liked VARCHAR(255), 
+	cuisines VARCHAR(255), 
+	approx_cost DECIMAL(8,2) NOT NULL, 
+	type VARCHAR(255)
+);
+
+
+SET GLOBAL local_infile=1;
+
+LOAD DATA LOCAL INFILE 'file_path'
+INTO TABLE zomato
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM zomato;
