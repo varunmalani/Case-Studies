203d92d696ee7fc308f6d9ce044e1e4502d6f086
Varun Malani <51241123+varunmalani@users.noreply.github.com>
2024-05-13 10:15:55 +0530

    Add files via upload

--- Zomato/Zomato_Cleaning.sql
+++ Zomato/Zomato_Cleaning.sql
@@ -1,4 +1,4 @@
-USE DATA_SCIENCE;
+-- https://www.kaggle.com/datasets/sanjanchaudhari/zomato-data
 
 CREATE TABLE zomato (
 	a INTEGER NOT NULL, 
@@ -15,7 +15,6 @@ CREATE TABLE zomato (
 	type VARCHAR(255)
 );
 
-
 SET GLOBAL local_infile=1;
 
 LOAD DATA LOCAL INFILE 'file_path'
@@ -24,5 +23,33 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+SELECT * FROM zomato;
+
+-- Fetching only the column names from the table zomato
+SELECT COLUMN_NAME FROM information_schema.columns
+WHERE TABLE_NAME = 'zomato'
+
+
+-- Cleaning the data
+
+-- We do not required column a, hence we can drop the column
+ALTER TABLE zomato DROP COLUMN a;
+
+-- Replace the BLANK with suitable values
+SELECT GROUP_CONCAT(
+	CONCAT('SUM(CASE WHEN `', COLUMN_NAME, '`='''' THEN 1 ELSE 0 END) AS `', COLUMN_NAME, '`')
+) INTO @ZomatoTblSummary
+FROM information_schema.columns
+WHERE TABLE_NAME = 'zomato';
+
+SELECT @ZomatoTblSummary;
+
+SET @ZomatoTblSummary = CONCAT('select ', @ZomatoTblSummary,' from zomato');
+
+PREPARE smt FROM  @ZomatoTblSummary;
+EXECUTE  smt ;
+DEALLOCATE  PREPARE smt;
+
 
 SELECT * FROM zomato;
